1e0258424bab965dee2594d0b6fd6d77bd06551b
samuelmhg97 <samuelmhg97@gmail.com>
2022-02-09 19:16:06 -0300

    homework actualized

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -27,7 +27,7 @@ function sumarArray(numeros, cb) {
   //Tu código:
   var suma = numeros.reduce(function(acc,elemento) {
     return acc + elemento; 
-  })
+  }, 0)
   cb(suma)
 }
 
@@ -35,9 +35,7 @@ function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
-  for (i=0; i<array.length; i++) {
-    cb(array[i]);
-  }
+  return array.forEach(cb);
 
 }
 
@@ -46,21 +44,23 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
-  const newArray = array.map(function(el) {
-    return cb(el);
-  }); return newArray;
+  const newArray = array.map(cb);
+  ; return newArray;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
-  var nuevoArray = [];
-  for (let i=0; i<array.length; i++) {
-    if(array[i][0] === "a") {
-      nuevoArray.push(array[i]);
-    }
-  } return nuevoArray;
+ // var nuevoArray = [];
+  //for (let i=0; i<array.length; i++) {
+   // if(array[i][0] === "a") {
+     // nuevoArray.push(array[i]);
+    //}
+  //} return nuevoArray;
+  const newArray  =array.filter(function(palabra){
+    return palabra[0] === "a";
+}); return newArray;
 }
 
 // No modificar nada debajo de esta línea
